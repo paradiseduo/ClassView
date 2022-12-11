b2dd3efe9880028a3897981687f2f8f2d6afafc8
Paradiseduo <>
2022-12-11 12:08:11 +0800

    新增WebViewHook

--- ClassView/Script/HookScript.swift
+++ ClassView/Script/HookScript.swift
@@ -483,3 +483,278 @@ class HookRequest: ScriptDelegate, FridaHook {
         }
     }
 }
+
+class HookWKWebView: ScriptDelegate, FridaHook {
+    private var script: Script?
+
+    func scriptDestroyed(_ script: Script) {
+        print("\(script) scriptDestroyed")
+    }
+
+    func script(_ script: Script, didReceiveMessage message: Any, withData data: Data?) {
+        
+    }
+    
+    func hook() {
+        if let session = USBDeviceManager.shared.session {
+            let s = """
+            const NSMutableDictionary = ObjC.classes.NSMutableDictionary;
+            const NSNumber = ObjC.classes.NSNumber;
+            const NSString = ObjC.classes.NSString;
+            const NSURLRequest = ObjC.classes.NSURLRequest;
+            const NSURLSession = ObjC.classes.NSURLSession;
+            const NSObject = ObjC.classes.NSObject;
+            const WKWebViewConfiguration = ObjC.classes.WKWebViewConfiguration;
+            const WKWebView = ObjC.classes.WKWebView;
+            
+            const cache = NSMutableDictionary.alloc().init();
+            const MyWKURLSchemeHandler = ObjC.registerClass({
+                name: 'MyWKURLSchemeHandler',
+                super: NSObject,
+                protocols: [ObjC.protocols.WKURLSchemeHandler],
+                methods: {
+                    '- init': function () {
+                        const self = this.super.init();
+                        if (self !== null) {
+                            ObjC.bind(self, { });
+                        }
+                        return self;
+                    },
+                    '- dealloc': function () {
+                        ObjC.unbind(this.self);
+                        this.super.dealloc();
+                    },
+                    '- webView:startURLSchemeTask:': function (webView, urlSchemeTask) {
+                        cache.setObject_forKey_("1", urlSchemeTask.description());
+                        var handler = new ObjC.Block({
+                            retType: 'void',
+                            argTypes: ['object', 'object', 'object'],
+                            implementation: function (data, response, error) {
+                                if (cache.objectForKey_(urlSchemeTask.description()) !== null) {
+                                    if (error) {
+                                        urlSchemeTask.didFailWithError_(error);
+                                    } else {
+                                        urlSchemeTask.didReceiveResponse_(response);
+                                        urlSchemeTask.didReceiveData_(data);
+                                        urlSchemeTask.didFinish();
+                                    }
+                                }
+                            }
+                        });
+                        NSURLSession.sharedSession().dataTaskWithRequest_completionHandler_(urlSchemeTask.request(), handler).resume();
+                    },
+                    '- webView:stopURLSchemeTask:': function (webView, urlSchemeTask) {
+                        cache.removeObjectForKey_(urlSchemeTask.description());
+                    }
+                }
+            });
+
+            function hook_WKWebView1() {
+                 const resolver = new ApiResolver('objc');
+                 resolver.enumerateMatches("+[* handlesURLScheme:]", {
+                     onMatch: function (i) {
+                         Interceptor.attach(i.address, {
+                             onEnter: function (args) {
+                                 this.pass = false;
+                                 var scheme = ObjC.Object(args[2]);
+                                 if (scheme.isEqualToString_("https") || scheme.isEqualToString_("http")) {
+                                     this.pass = true;
+                                 }
+                             },
+                             onLeave(retval) {
+                                 if (this.pass) {
+                                     retval.replace(0x0);
+                                 }
+                             }
+                         });
+                     },
+                     onComplete: function () {
+                        hook_WKWebView2();
+                     }
+                 });
+            }
+
+
+            function hook_WKWebView2() {
+                var hook = WKWebView['- initWithFrame:configuration:'];
+                Interceptor.attach(hook.implementation, {
+                    onEnter: function (args) {
+                        var config = WKWebViewConfiguration.alloc().init();
+                        if (config !== null) {
+                            config = ObjC.Object(args[2]).copy();
+                        }
+                        if (config.urlSchemeHandlerForURLScheme_("http") !== true && WKWebView.handlesURLScheme_("http") !== true) {
+                            config.setURLSchemeHandler_forURLScheme_(MyWKURLSchemeHandler.alloc().init(), "http");
+                        }
+                        if (config.urlSchemeHandlerForURLScheme_("https") !== true && WKWebView.handlesURLScheme_("https") !== true) {
+                            config.setURLSchemeHandler_forURLScheme_(MyWKURLSchemeHandler.alloc().init(), "https");
+                        }
+                        args[2] = config;
+                    }
+                });
+            }
+
+            hook_WKWebView1();
+            ObjC.schedule(ObjC.mainQueue, function () {
+                
+            });
+
+            """
+            session.createScript(s, name: "HookWKWebView", runtime: ScriptRuntime.auto) { scriptResult in
+                do {
+                    self.script = try scriptResult()
+                    self.script?.delegate = self
+                    self.script?.load() { result in
+                        do {
+                            if try result() {
+                                print("Script HookWKWebView loaded")
+                            } else {
+                                print("Script HookWKWebView loaded failed")
+                            }
+                        } catch let e {
+                            print(e)
+                        }
+                    }
+                } catch let e {
+                    print(e)
+                }
+            }
+        } else {
+            print("USBDeviceManager.shared.session not found")
+        }
+    }
+}
+
+
+class HookUIView: ScriptDelegate, FridaHook {
+    private var script: Script?
+
+    func scriptDestroyed(_ script: Script) {
+        print("\(script) scriptDestroyed")
+    }
+
+    func script(_ script: Script, didReceiveMessage message: Any, withData data: Data?) {
+        if let dic = message as? NSDictionary {
+            if let p = dic["payload"] as? String {
+                let arr = p.components(separatedBy: "ƒ")
+                let className = arr[0]
+                let labelText = arr[1]
+                let imageStr = arr[2]
+                if let d = Data(base64Encoded: imageStr), let image = NSImage(data: d) {
+                    print(image)
+                }
+            }
+        }
+    }
+    
+    func hook() {
+        if let session = USBDeviceManager.shared.session {
+            let s = """
+            const NSMutableDictionary = ObjC.classes.NSMutableDictionary;
+            const NSData = ObjC.classes.NSData;
+            const NSString = ObjC.classes.NSString;
+            const UILabel = ObjC.classes.UILabel;
+            const UIView = ObjC.classes.UIView;
+            const UIViewController = ObjC.classes.UIViewController;
+            const UIApplication = ObjC.classes.UIApplication;
+            const UIScreen = ObjC.classes.UIScreen;
+            const NSTimer = ObjC.classes.NSTimer;
+            const paradise_labelDic = NSMutableDictionary.alloc().init();
+
+            function screenshot(labelText, className) {
+                paradise_labelDic.setObject_forKey_(labelText, className);
+                const handler = new ObjC.Block({
+                    retType: 'void',
+                    argTypes: ['object'],
+                    implementation() {
+                        var getNativeFunction = function (ex, retVal, args) {
+                            return new NativeFunction(Module.findExportByName('UIKit', ex), retVal, args);
+                        };
+                        var api = {
+                            UIWindow: ObjC.classes.UIWindow,
+                            UIGraphicsBeginImageContextWithOptions: getNativeFunction('UIGraphicsBeginImageContextWithOptions', 'void', [['double', 'double'], 'bool', 'double']),
+                            UIGraphicsBeginImageContextWithOptions: getNativeFunction('UIGraphicsBeginImageContextWithOptions', 'void', [['double', 'double'], 'bool', 'double']),
+                            UIGraphicsEndImageContext: getNativeFunction('UIGraphicsEndImageContext', 'void', []),
+                            UIGraphicsGetImageFromCurrentImageContext: getNativeFunction('UIGraphicsGetImageFromCurrentImageContext', 'pointer', []),
+                            UIImagePNGRepresentation: getNativeFunction('UIImagePNGRepresentation', 'pointer', ['pointer']),
+                            UIImageJPEGRepresentation: getNativeFunction('UIImageJPEGRepresentation', 'pointer', ['pointer', 'double'])
+                        };
+                        var view = api.UIWindow.keyWindow();
+                        var bounds = view.bounds();
+                        var size = bounds[1];
+                        api.UIGraphicsBeginImageContextWithOptions(size, 0, 0);
+                        view.drawViewHierarchyInRect_afterScreenUpdates_(bounds, true);
+                
+                        var image = api.UIGraphicsGetImageFromCurrentImageContext();
+                        api.UIGraphicsEndImageContext();
+                
+                        var png = new ObjC.Object(api.UIImageJPEGRepresentation(image, 0.5));
+                        var str = png.base64EncodedStringWithOptions_(0);
+                        console.log(className + "ƒ" + labelText + "ƒ" + str);
+                    }
+                });
+                ObjC.schedule(ObjC.mainQueue, function () {
+                    NSTimer.scheduledTimerWithTimeInterval_repeats_block_(0.25, 0x0, handler);
+                });
+            }
+
+            function hook_layoutSubview() {
+                var hook = UIView['- layoutSubviews'];
+                Interceptor.attach(hook.implementation, {
+                    onEnter: function (args) {
+                        var self = ObjC.Object(args[0]);
+                        if (self.class().isSubclassOfClass_(UILabel.class())) {
+                            var s = self.text();
+                            var next = self.nextResponder();
+                            while (next !== null) {
+                                if (next.isKindOfClass_(UIViewController.class()) || next.$className === "UIApplication") {
+                                    var className = next.class().toString()
+                                    if (paradise_labelDic.valueForKey_(className) === null) {
+                                        if (s.containsString_("隐私权政策")) {
+                                            screenshot("隐私权政策", className);
+                                        }
+                                        if (s.containsString_("用户名")) {
+                                            screenshot("用户名", className);
+                                            paradise_labelDic.setObject_forKey_("1", next.class().toString());
+                                        }
+                                        if (s.containsString_("密码")) {
+                                            screenshot("密码", className);
+                                        }
+                                    }
+                                    break;
+                                }
+                                next = next.nextResponder();
+                            }
+                        }
+                    }
+                });
+            }
+
+            ObjC.schedule(ObjC.mainQueue, function () {
+                hook_layoutSubview();
+            });
+            """
+            session.createScript(s, name: "HookUIView", runtime: ScriptRuntime.auto) { scriptResult in
+                do {
+                    self.script = try scriptResult()
+                    self.script?.delegate = self
+                    self.script?.load() { result in
+                        do {
+                            if try result() {
+                                print("Script HookUIView loaded")
+                            } else {
+                                print("Script HookUIView loaded failed")
+                            }
+                        } catch let e {
+                            print(e)
+                        }
+                    }
+                } catch let e {
+                    print(e)
+                }
+            }
+        } else {
+            print("USBDeviceManager.shared.session not found")
+        }
+    }
+}
